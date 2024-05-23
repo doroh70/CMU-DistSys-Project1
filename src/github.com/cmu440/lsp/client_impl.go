3816a749efd6453221408d0186959c3a10badec1
doroh70 <oghenedavid123@gmail.com>
2024-05-22 23:18:13 -0600

    client checkpoint A

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,39 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/cmu440/lspnet"
+	"time"
+)
 
 type client struct {
-	// TODO: implement this!
+	activeRoutines                     int
+	beenClosed                         bool
+	clientSeqNumber                    int
+	serverSeqNumber                    int
+	conn                               *lspnet.UDPConn
+	connID                             int
+	serverMessagesMap                  map[int]*Message
+	unAckedClientMessagesMap           map[int]*Message
+	incrementNumActiveRoutinesChan     chan bool
+	decrementNumActiveRoutinesChan     chan bool
+	requestBeenClosedChan              chan bool
+	responseBeenClosedChan             chan bool
+	canSafelyCloseChan                 chan bool
+	closeServerChan                    chan bool
+	requestConnIDChan                  chan bool
+	responseConnIDChan                 chan int
+	requestSequencedServerMessageChan  chan bool
+	responseSequencedServerMessageChan chan *Message
+	requestNewSequenceNumChan          chan bool
+	responseNewSequenceNumChan         chan int
+	stopReadingChan                    chan bool
+	stopWritingChan                    chan bool
+	serverMessageChan                  chan *Message
+	writeChan                          chan *Message
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -22,23 +51,331 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, initialSeqNum int, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	const INITSEQUENCENUMBER = 1
+	const NUMRETRIES = 5
+	addr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	//bind local socket to remote address
+	conn, err := lspnet.DialUDP("udp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+	//send conn message
+	msg := NewConnect(INITSEQUENCENUMBER)
+	marshalledConn, err := json.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	//Try sending multiple times
+	for i := 0; i < NUMRETRIES; i++ {
+		if _, err1 := conn.Write(marshalledConn); err1 == nil {
+			break
+		} else if i == NUMRETRIES-1 {
+			return nil, err1
+		}
+	}
+	//wait for conn ack
+	var ack *Message
+	buffer := make([]byte, 1024)
+	bytesRead, err := conn.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+	//Trim the buffer to the number of valid bytes read
+	validBuffer := buffer[:bytesRead]
+	ack = &Message{}
+	err = json.Unmarshal(validBuffer, ack)
+	if err != nil {
+		return nil, err
+	}
+	if ack.Type != MsgAck || ack.SeqNum != INITSEQUENCENUMBER {
+		return nil, fmt.Errorf("invalid acknowledgment received")
+	}
+	//instantiate client
+	client := &client{
+		activeRoutines:                     0,
+		beenClosed:                         false,
+		clientSeqNumber:                    INITSEQUENCENUMBER,
+		serverSeqNumber:                    INITSEQUENCENUMBER + 1, //per doc
+		conn:                               conn,
+		connID:                             ack.ConnID,
+		serverMessagesMap:                  make(map[int]*Message),
+		unAckedClientMessagesMap:           make(map[int]*Message),
+		incrementNumActiveRoutinesChan:     make(chan bool),
+		decrementNumActiveRoutinesChan:     make(chan bool),
+		requestConnIDChan:                  make(chan bool),
+		responseConnIDChan:                 make(chan int),
+		closeServerChan:                    make(chan bool),
+		requestBeenClosedChan:              make(chan bool),
+		responseBeenClosedChan:             make(chan bool),
+		canSafelyCloseChan:                 make(chan bool, 1),
+		requestSequencedServerMessageChan:  make(chan bool),
+		responseSequencedServerMessageChan: make(chan *Message),
+		requestNewSequenceNumChan:          make(chan bool),
+		responseNewSequenceNumChan:         make(chan int),
+		stopReadingChan:                    make(chan bool),
+		stopWritingChan:                    make(chan bool),
+		serverMessageChan:                  make(chan *Message),
+		writeChan:                          make(chan *Message),
+	}
+
+	//start client worker routine
+	go client.clientWorkerRoutine()
+	//start reader routine
+	go client.readRoutine()
+	//start writer routine
+	go client.writeRoutine()
+	return client, nil
+}
+
+func (c *client) runWithActiveRoutine(fn func()) {
+	c.incrementNumActiveRoutinesChan <- true
+	defer func() { c.decrementNumActiveRoutinesChan <- true }()
+	fn()
+}
+
+func (c *client) checkClosed() bool {
+	//if client has been closed return error
+	c.requestBeenClosedChan <- true
+	closedb4 := <-c.responseBeenClosedChan
+	if closedb4 {
+		return true
+	}
+	return false
+}
+
+func (c *client) closeProc() {
+	// Mark the client as having been closed to prevent new operations
+	c.beenClosed = true
+
+	// Wait for all pending outgoing messages to be acknowledged
+	ticker := time.NewTicker(100 * time.Millisecond) // Check every 100ms
+	defer ticker.Stop()
+
+	for range ticker.C {
+		if len(c.unAckedClientMessagesMap) == 0 {
+			break // Break the loop when no unacknowledged messages are left
+		}
+	}
+
+	// Stop reading and writing routines by signaling stop channels
+	c.stopReadingChan <- true
+	c.stopWritingChan <- true
+
+	// Wait for the routines to confirm they have stopped
+	<-c.canSafelyCloseChan
+
+}
+
+func (c *client) clientWorkerRoutine() {
+	c.activeRoutines++
+	defer func() { c.activeRoutines-- }()
+	for {
+		select {
+		case <-c.incrementNumActiveRoutinesChan:
+			c.activeRoutines++
+		case <-c.decrementNumActiveRoutinesChan:
+			c.activeRoutines--
+		case <-c.requestBeenClosedChan:
+			c.responseBeenClosedChan <- c.beenClosed
+		case <-c.closeServerChan:
+			c.closeProc()
+			return
+		case <-c.requestConnIDChan:
+			c.responseConnIDChan <- c.connID
+		case <-c.requestSequencedServerMessageChan:
+			//Search serverMessagesMap for the message with the current serverSeqNumber
+			msg, found := c.serverMessagesMap[c.serverSeqNumber]
+			if found {
+				//If the message is present, send the message through the channel
+				c.responseSequencedServerMessageChan <- msg
+				//Increment the server sequence number
+				c.serverSeqNumber++
+			} else {
+				//If no message is found, send a nil pointer
+				c.responseSequencedServerMessageChan <- nil
+			}
+		case <-c.requestNewSequenceNumChan:
+			c.clientSeqNumber++
+			c.responseNewSequenceNumChan <- c.clientSeqNumber
+		case msg := <-c.serverMessageChan:
+			//if we already have that message in map, send ack and continue
+			if _, exists := c.serverMessagesMap[msg.SeqNum]; exists {
+				ack := NewAck(c.connID, msg.SeqNum)
+				c.writeChan <- ack
+				continue
+			} else {
+				switch msg.Type {
+				case MsgAck:
+					//Delete singular message from unAckedClientMessagesMap
+					if _, exists := c.unAckedClientMessagesMap[msg.SeqNum]; exists {
+						delete(c.unAckedClientMessagesMap, msg.SeqNum)
+						fmt.Println("Ack received, message removed:", msg.SeqNum)
+					} else {
+						fmt.Println("Ack received for non-existent message:", msg.SeqNum)
+					}
+				case MsgCAck:
+					//Delete multiple messages from unAckedClientMessagesMap up to the given sequence number
+					for seqNum := range c.unAckedClientMessagesMap {
+						if seqNum <= msg.SeqNum {
+							delete(c.unAckedClientMessagesMap, seqNum)
+						}
+					}
+					fmt.Printf("CAck received, messages up to %d removed\n", msg.SeqNum)
+				case MsgData:
+					//verify checksum, if valid add to serverMessagesMap
+					checkSum := CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload)
+					if checkSum == msg.Checksum {
+						//If checksum is valid, add message to serverMessagesMap
+						c.serverMessagesMap[msg.SeqNum] = msg
+						//Then send acknowledgment back to sender
+						ack := NewAck(msg.ConnID, msg.SeqNum)
+						c.writeChan <- ack
+					} else {
+						//Handle the case where the checksum does not match
+						fmt.Printf("Checksum verification failed for server message : %s\n", msg)
+						continue
+					}
+				default:
+					//do nothing, shouldn't get here
+					fmt.Printf("Unexpected message type received from server: %s\n", msg)
+				}
+			}
+		}
+	}
+}
+
+func (c *client) writeRoutine() {
+	c.runWithActiveRoutine(func() {
+		for {
+			select {
+			case <-c.stopWritingChan:
+				return
+			case msg := <-c.writeChan:
+				// Convert the message into JSON format
+				byteMsg, err := json.Marshal(msg)
+				if err != nil {
+					fmt.Println("Error marshalling JSON:", err)
+					continue // Skip this message if marshalling fails
+				}
+
+				maxRetries := 3
+				for attempt := 0; attempt < maxRetries; attempt++ {
+					nbytes, err := c.conn.Write(byteMsg)
+					if err != nil {
+						fmt.Printf("Error writing to socket: %v, retrying %d of %d\n", err, attempt+1, maxRetries)
+						continue // Retry sending the message
+					}
+					if nbytes != len(byteMsg) {
+						fmt.Printf("Warning: Only %d out of %d bytes sent, retrying %d of %d\n", nbytes, len(byteMsg), attempt+1, maxRetries)
+						continue // Retry sending the message
+					}
+					// Exit the loop if the message is sent successfully
+					break
+				}
+
+				//Check if message type is MsgData and if the message doesn't already exist in the map
+				if msg.Type == MsgData {
+					if _, exists := c.unAckedClientMessagesMap[msg.SeqNum]; !exists {
+						//Add to unAckedMap
+						c.unAckedClientMessagesMap[msg.SeqNum] = msg
+					}
+				}
+			}
+		}
+	})
+}
+
+func (c *client) readRoutine() {
+	c.runWithActiveRoutine(func() {
+		for {
+			select {
+			case <-c.stopReadingChan:
+				return
+			default:
+				var msg *Message
+				buffer := make([]byte, 1024)
+				for {
+					bytesRead, err := c.conn.Read(buffer)
+					if err == nil {
+						validBuffer := buffer[:bytesRead]
+						msg = &Message{}
+						if err := json.Unmarshal(validBuffer, msg); err == nil {
+							c.serverMessageChan <- msg
+						} else {
+							fmt.Println("Error unmarshalling JSON:", err)
+						}
+						break
+					}
+					fmt.Println("Error reading from connection:", err)
+					break
+				}
+			}
+		}
+	})
 }
 
 func (c *client) ConnID() int {
-	return -1
+	c.requestConnIDChan <- true
+	id := <-c.responseConnIDChan
+	return id
 }
 
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+	//if conn has been closed return error
+	if closedb4 := c.checkClosed(); closedb4 {
+		return nil, errors.New("server has already been closed")
+	}
+	//request for message in order of seq #
+	var seqServerDataMsg *Message
+	for {
+		c.requestSequencedServerMessageChan <- true
+		seqServerDataMsg = <-c.responseSequencedServerMessageChan
+		if seqServerDataMsg != nil {
+			break
+		}
+	}
+	//return message
+	return seqServerDataMsg.Payload, nil
 }
 
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	//if conn has been closed return error
+	if closedb4 := c.checkClosed(); closedb4 {
+		return errors.New("server has already been closed")
+	}
+	//construct data message
+	c.requestNewSequenceNumChan <- true
+	seqNum := <-c.responseNewSequenceNumChan
+	checkSum := CalculateChecksum(c.ConnID(), seqNum, len(payload), payload)
+	dataMsg := NewData(c.ConnID(), seqNum, len(payload), payload, checkSum)
+	//send to writer routine
+	c.writeChan <- dataMsg
+	return nil
 }
 
 func (c *client) Close() error {
-	return errors.New("not yet implemented")
+	//if conn has been closed return error
+	if closedb4 := c.checkClosed(); closedb4 {
+		return errors.New("server has already been closed")
+	}
+	//signal server has been closed
+	c.closeServerChan <- true
+	//all pending outgoing messages should be sent and acked ~ wait until unAcked buffer is empty and writes are sent
+	<-c.canSafelyCloseChan
+	//sanity checking all spawned routines are closed
+	for {
+		if c.activeRoutines == 0 {
+			break
+		}
+	}
+	//close LSP socket
+	err := c.conn.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
