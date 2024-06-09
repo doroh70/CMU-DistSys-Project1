a23c8c1a64ea72dbbd1da8b1613cbfe75c048e5c
doroh70 <oghenedavid123@gmail.com>
2024-06-09 14:36:19 -0600

    one more routine left

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,34 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/cmu440/lspnet"
+)
 
 type client struct {
-	// TODO: implement this!
+	conn                       *lspnet.UDPConn
+	connectionId               int
+	sequenceNumber             int
+	params                     *Params
+	allRoutinesTerminatedChan  chan bool
+	closeClientChan            chan bool
+	closeReadRoutineChan       chan bool
+	connEstablishedChan        chan bool
+	inboundMessageChan         chan *Message
+	requestBeenClosedChan      chan bool
+	responseBeenClosedChan     chan bool
+	requestConnLostChan        chan bool
+	responseConnLostChan       chan bool
+	requestMessageExistsChan   chan bool
+	responseMessageExistsChan  chan bool
+	requestNewSequenceNumChan  chan bool
+	responseNewSequenceNumChan chan int
+	requestOrderedMessageChan  chan bool
+	responseOrderedMessageChan chan *Message
+	writeChan                  chan *Message
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -22,23 +46,160 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, initialSeqNum int, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	// Open up UDP connection
+	addr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	//bind local socket to remote address
+	conn, err := lspnet.DialUDP("udp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+	// Instantiate client
+	client := &client{
+		conn:                 conn,
+		closeReadRoutineChan: make(chan bool),
+		connEstablishedChan:  make(chan bool),
+		inboundMessageChan:   make(chan *Message),
+		sequenceNumber:       initialSeqNum,
+		params:               params,
+	}
+	// Launch read, and worker routines
+	go client.clientWorkerRoutine()
+	go client.readRoutine()
+	// block on signal that indicates whether LSP connection was established or not
+	madeGood := <-client.connEstablishedChan
+	if madeGood {
+		return client, nil
+	}
+	client.Close()
+	return nil, errors.New("unable to establish connection")
 }
 
+// ConnID returns the connection ID of the client. It is immutable after being set initially.
 func (c *client) ConnID() int {
-	return -1
+	return c.connectionId
 }
 
+// Read will hang if server has already been explicitly closed
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+	// If Close has been called on the client, subsequent calls to Read must either return a non-nil error, or never return anything.
+	c.requestBeenClosedChan <- true
+	beenClosed := <-c.responseBeenClosedChan
+	if beenClosed {
+		return nil, errors.New("client has been closed")
+	}
+	// Return a non-nil error if the connection with the server has been lost and no other messages are waiting to be returned
+	c.requestConnLostChan <- true
+	connLost := <-c.responseConnLostChan
+	if connLost {
+		c.requestMessageExistsChan <- true
+		exists := <-c.responseMessageExistsChan
+		if !exists {
+			return nil, errors.New("connection has been lost")
+		}
+	}
+	c.requestOrderedMessageChan <- true
+	msg := <-c.responseOrderedMessageChan
+	return msg.Payload, nil
+	// (3) the server is closed ~ this is ambiguous af, so will ignore for now. Only thing I can think of right now that would check for this is a ICMP message
 }
 
+// Write will hang if server has already been explicitly closed
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	// return a non-nil error if the connection with the server has been lost
+	c.requestConnLostChan <- true
+	connLost := <-c.responseConnLostChan
+	if connLost {
+		return errors.New("connection has been lost")
+	}
+	// If Close has been called on the client, subsequent calls to Write must either return a non-nil error, or never return anything.
+	c.requestBeenClosedChan <- true
+	beenClosed := <-c.responseBeenClosedChan
+	if beenClosed {
+		return errors.New("client has been closed")
+	}
+	// construct data message
+	c.requestNewSequenceNumChan <- true
+	seqNum := <-c.responseNewSequenceNumChan
+	checkSum := CalculateChecksum(c.ConnID(), seqNum, len(payload), payload)
+	dataMsg := NewData(c.ConnID(), seqNum, len(payload), payload, checkSum)
+	// offload to worker routine
+	c.writeChan <- dataMsg
+	return nil
 }
 
+// Close will hang if server has already been explicitly closed
 func (c *client) Close() error {
-	return errors.New("not yet implemented")
+	c.closeClientChan <- true
+	<-c.allRoutinesTerminatedChan
+	return nil
+}
+
+func (c *client) clientWorkerRoutine() {
+	// This should only return when all spawned routines are terminated
+	defer func() { c.allRoutinesTerminatedChan <- true }()
+	for {
+		select {
+		case <-c.closeClientChan:
+			// insert closeProcedure
+			return
+		case <-c.writeChan:
+			// Put into queue to be admitted into session sliding window
+		}
+	}
+}
+
+// readRoutine continuously reads LSP packets from the UDP connection, and passes them to the worker routine for handling
+func (c *client) readRoutine() {
+	for {
+		select {
+		case <-c.closeReadRoutineChan:
+			return
+		default:
+			c.readAndProcessPacket()
+		}
+	}
+}
+
+// readAndProcessPacket reads a packet from the UDP connection and processes it
+func (c *client) readAndProcessPacket() {
+	buffer := make([]byte, 2000)
+	bytesRead, err := c.conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection:", err)
+		return
+	}
+
+	validBuffer := buffer[:bytesRead]
+	msg, err := unmarshalAndVerifyMessage(validBuffer)
+	if err != nil {
+		fmt.Println("Error processing message:", err)
+		return
+	}
+
+	c.inboundMessageChan <- msg
+}
+
+// unmarshalAndVerifyMessage unmarshals the buffer into a Message and verifies its integrity
+func unmarshalAndVerifyMessage(buffer []byte) (*Message, error) {
+	msg := &Message{}
+	if err := json.Unmarshal(buffer, msg); err != nil {
+		return nil, fmt.Errorf("unmarshal error: %w", err)
+	}
+
+	// Verify payload size
+	if msg.Size > len(msg.Payload) {
+		return nil, fmt.Errorf("payload size mismatch")
+	}
+
+	msg.Payload = msg.Payload[:msg.Size]
+
+	// Verify checksum
+	if msg.Checksum != CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload) {
+		return nil, fmt.Errorf("checksum mismatch")
+	}
+
+	return msg, nil
 }
