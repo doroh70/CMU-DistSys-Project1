f51145cfe503c3cdc9079f2037ec603dfebecf5e
doroh70 <oghenedavid123@gmail.com>
2024-06-09 23:46:14 -0600

    epoch for establish conn

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -7,17 +7,20 @@ import (
 	"errors"
 	"fmt"
 	"github.com/cmu440/lspnet"
+	"time"
 )
 
 type client struct {
+	beenClosed                 bool
 	conn                       *lspnet.UDPConn
 	connectionId               int
+	isConnLost                 bool
 	sequenceNumber             int
 	params                     *Params
 	allRoutinesTerminatedChan  chan bool
 	closeClientChan            chan bool
 	closeReadRoutineChan       chan bool
-	connEstablishedChan        chan bool
+	connEstablishedChan        chan bool // connEstablishedChan signals to startup routine via NewClient method, whether a connection with the server was established or not. A false signal would trigger the Close client method.
 	inboundMessageChan         chan *Message
 	requestBeenClosedChan      chan bool
 	responseBeenClosedChan     chan bool
@@ -51,29 +54,48 @@ func NewClient(hostport string, initialSeqNum int, params *Params) (Client, erro
 	if err != nil {
 		return nil, err
 	}
-	//bind local socket to remote address
+	// bind local socket to remote address
 	conn, err := lspnet.DialUDP("udp", nil, addr)
 	if err != nil {
 		return nil, err
 	}
 	// Instantiate client
 	client := &client{
-		conn:                 conn,
-		closeReadRoutineChan: make(chan bool),
-		connEstablishedChan:  make(chan bool),
-		inboundMessageChan:   make(chan *Message),
-		sequenceNumber:       initialSeqNum,
-		params:               params,
+		beenClosed:                 false,
+		conn:                       conn,
+		connectionId:               0,
+		isConnLost:                 false,
+		sequenceNumber:             initialSeqNum,
+		params:                     params,
+		allRoutinesTerminatedChan:  make(chan bool),
+		closeClientChan:            make(chan bool),
+		closeReadRoutineChan:       make(chan bool),
+		connEstablishedChan:        make(chan bool),
+		inboundMessageChan:         make(chan *Message),
+		requestBeenClosedChan:      make(chan bool),
+		responseBeenClosedChan:     make(chan bool),
+		requestConnLostChan:        make(chan bool),
+		responseConnLostChan:       make(chan bool),
+		requestMessageExistsChan:   make(chan bool),
+		responseMessageExistsChan:  make(chan bool),
+		requestNewSequenceNumChan:  make(chan bool),
+		responseNewSequenceNumChan: make(chan int),
+		requestOrderedMessageChan:  make(chan bool),
+		responseOrderedMessageChan: make(chan *Message),
+		writeChan:                  make(chan *Message),
 	}
 	// Launch read, and worker routines
 	go client.clientWorkerRoutine()
 	go client.readRoutine()
-	// block on signal that indicates whether LSP connection was established or not
+
+	// Block on signal that indicates whether LSP connection was established or not
 	madeGood := <-client.connEstablishedChan
 	if madeGood {
 		return client, nil
 	}
-	client.Close()
+
+	// Close the client if the connection was not established
+	_ = client.Close()
 	return nil, errors.New("unable to establish connection")
 }
 
@@ -140,13 +162,63 @@ func (c *client) Close() error {
 func (c *client) clientWorkerRoutine() {
 	// This should only return when all spawned routines are terminated
 	defer func() { c.allRoutinesTerminatedChan <- true }()
+
+	// Attempt to send the connection message to the server with retry
+	err := c.retrySendConnect(3) // 3 attempts
+	if err != nil {
+		fmt.Println("Failed to establish connection:", err)
+		c.connEstablishedChan <- false // this will go on to close the server ~ look up NewClient method
+	}
+	connAckReceived := false
+	currentEpochsElapsed := 0
+
+	epochTimer := time.NewTimer(time.Duration(c.params.EpochMillis) * time.Millisecond)
 	for {
 		select {
 		case <-c.closeClientChan:
-			// insert closeProcedure
+			// insert close Procedure
 			return
+		case <-c.closeReadRoutineChan:
+			// insert close readRoutine procedure
 		case <-c.writeChan:
-			// Put into queue to be admitted into session sliding window
+			// insert write procedure
+		case <-c.inboundMessageChan:
+			// insert inbound message procedure
+		case <-c.requestBeenClosedChan:
+			c.responseBeenClosedChan <- c.beenClosed
+		case <-c.requestConnLostChan:
+			c.responseConnLostChan <- c.isConnLost
+		case <-c.requestMessageExistsChan:
+			// insert message exists query procedure
+		case <-c.requestNewSequenceNumChan:
+			c.sequenceNumber++
+			c.responseNewSequenceNumChan <- c.sequenceNumber
+		case <-c.responseOrderedMessageChan:
+			// insert ordered message read
+		case <-c.writeChan:
+			// insert write procedure
+		case <-epochTimer.C:
+			// insert epoch procedure
+
+			// If we haven't received a connection acknowledgment, attempt to resend
+			if !connAckReceived {
+				if currentEpochsElapsed < c.params.EpochLimit {
+					// Retry sending the connection request up to 3 times
+					err := c.retrySendConnect(3)
+					if err != nil {
+						fmt.Println("Retry failed with error:", err)
+					}
+					// Increment the epochs elapsed counter
+					currentEpochsElapsed++
+				} else {
+					// If the epoch limit is reached, signal that the connection could not be established
+					fmt.Println("Epoch limit reached. Could not establish connection.")
+					c.connEstablishedChan <- false
+				}
+			}
+
+			//Else regular epoch procedure
+
 		}
 	}
 }
@@ -202,4 +274,23 @@ func unmarshalAndVerifyMessage(buffer []byte) (*Message, error) {
 	}
 
 	return msg, nil
+}
+
+// retrySendConnect attempts to send the connection message up to maxRetries times.
+func (c *client) retrySendConnect(maxRetries int) error {
+	connMsg := NewConnect(c.sequenceNumber)
+	marshalledConn, _ := json.Marshal(connMsg)
+
+	var err error
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		_, err = c.conn.Write(marshalledConn)
+		if err == nil {
+			// If write is successful, exit the loop
+			return nil
+		}
+		fmt.Printf("Attempt %d: Error sending connection message: %v\n", attempt, err)
+		time.Sleep(1 * time.Second) // Wait before the next retry
+	}
+	// Return the error after maxRetries attempts
+	return fmt.Errorf("after %d attempts, failed to send connection message: %v", maxRetries, err)
 }
